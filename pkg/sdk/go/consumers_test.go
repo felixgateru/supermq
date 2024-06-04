1ce0e32084070027de61ba5555db88442adec9f3
1998-felix <felix.gateru@gmail.com>
2024-06-04 15:17:36 +0300

    feat: update notifiers tests
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

--- pkg/sdk/go/consumers_test.go
+++ pkg/sdk/go/consumers_test.go
@@ -9,48 +9,55 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/absmach/magistrala"
-	authmocks "github.com/absmach/magistrala/auth/mocks"
 	"github.com/absmach/magistrala/consumers/notifiers"
 	httpapi "github.com/absmach/magistrala/consumers/notifiers/api"
-	"github.com/absmach/magistrala/consumers/notifiers/mocks"
+	notmocks "github.com/absmach/magistrala/consumers/notifiers/mocks"
 	"github.com/absmach/magistrala/internal/apiutil"
 	"github.com/absmach/magistrala/internal/testsutil"
 	mglog "github.com/absmach/magistrala/logger"
 	"github.com/absmach/magistrala/pkg/errors"
 	svcerr "github.com/absmach/magistrala/pkg/errors/service"
 	sdk "github.com/absmach/magistrala/pkg/sdk/go"
-	"github.com/absmach/magistrala/pkg/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 var (
-	sub1 = sdk.Subscription{
+	ownerID   = testsutil.GenerateUUID(&testing.T{})
+	subID     = testsutil.GenerateUUID(&testing.T{})
+	sdkSubReq = sdk.Subscription{
 		Topic:   "topic",
 		Contact: "contact",
 	}
-	emptySubscription = sdk.Subscription{}
-	exampleUser1      = "email1@example.com"
-	ID                = testsutil.GenerateUUID(&testing.T{})
+	sdkSubRes = sdk.Subscription{
+		Topic:   "topic",
+		Contact: "contact",
+		OwnerID: ownerID,
+		ID:      subID,
+	}
+	notSubReq = notifiers.Subscription{
+		Contact: "contact",
+		Topic:   "topic",
+	}
+	notSubRes = notifiers.Subscription{
+		Contact: "contact",
+		Topic:   "topic",
+		OwnerID: ownerID,
+		ID:      subID,
+	}
+	exampleUser1 = "email1@example.com"
 )
 
-func setupSubscriptions() (*httptest.Server, *authmocks.AuthClient, *mocks.SubscriptionsRepository) {
-	repo := new(mocks.SubscriptionsRepository)
-	auth := new(authmocks.AuthClient)
-	notifier := new(mocks.Notifier)
-	idp := uuid.NewMock()
-	from := "exampleFrom"
-
-	svc := notifiers.New(auth, repo, idp, notifier, from)
+func setupSubscriptions() (*httptest.Server, *notmocks.Service) {
+	nsvc := new(notmocks.Service)
 	logger := mglog.NewMock()
-	mux := httpapi.MakeHandler(svc, logger, instanceID)
+	mux := httpapi.MakeHandler(nsvc, logger, instanceID)
 
-	return httptest.NewServer(mux), auth, repo
+	return httptest.NewServer(mux), nsvc
 }
 
 func TestCreateSubscription(t *testing.T) {
-	ts, auth, repo := setupSubscriptions()
+	ts, nsvc := setupSubscriptions()
 	defer ts.Close()
 
 	sdkConf := sdk.Config{
@@ -65,65 +72,86 @@ func TestCreateSubscription(t *testing.T) {
 		desc         string
 		subscription sdk.Subscription
 		token        string
-		err          errors.SDKError
 		empty        bool
 		id           string
-		identifyErr  error
-		userID       string
+		svcReq       notifiers.Subscription
+		svcErr       error
+		svcRes       string
+		err          errors.SDKError
 	}{
 		{
 			desc:         "create new subscription",
-			subscription: sub1,
-			token:        exampleUser1,
-			err:          nil,
+			subscription: sdkSubReq,
+			token:        validToken,
 			empty:        false,
-			id:           ID,
-			identifyErr:  nil,
-			userID:       validID,
+			svcReq:       notSubReq,
+			svcRes:       subID,
+			svcErr:       nil,
+			err:          nil,
 		},
 		{
 			desc:         "create new subscription with empty token",
-			subscription: sub1,
+			subscription: sdkSubReq,
 			token:        "",
-			err:          errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
 			empty:        true,
-			id:           "",
-			identifyErr:  svcerr.ErrAuthorization,
+			svcReq:       notifiers.Subscription{},
+			svcRes:       "",
+			svcErr:       nil,
+			err:          errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
 		},
 		{
 			desc:         "create new subscription with invalid token",
-			subscription: sub1,
-			token:        authmocks.InvalidValue,
-			err:          errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
+			subscription: sdkSubReq,
+			token:        invalidToken,
 			empty:        true,
-			id:           "",
-			identifyErr:  svcerr.ErrAuthorization,
+			svcReq:       notSubReq,
+			svcRes:       "",
+			svcErr:       svcerr.ErrAuthentication,
+			err:          errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
 		},
 		{
-			desc:         "create new empty subscription",
-			subscription: emptySubscription,
-			token:        token,
-			err:          errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrInvalidTopic), http.StatusBadRequest),
-			empty:        true,
-			id:           "",
-			identifyErr:  nil,
-			userID:       validID,
+			desc: "create new subscription with empty topic",
+			subscription: sdk.Subscription{
+				Topic:   "",
+				Contact: "contact",
+			},
+			token:  validToken,
+			empty:  true,
+			svcReq: notifiers.Subscription{},
+			svcErr: nil,
+			svcRes: "",
+			err:    errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrInvalidTopic), http.StatusBadRequest),
+		},
+		{
+			desc: "create new subscription with empty contact",
+			subscription: sdk.Subscription{
+				Topic:   "topic",
+				Contact: "",
+			},
+			token:  validToken,
+			empty:  true,
+			svcReq: notifiers.Subscription{},
+			svcErr: nil,
+			svcRes: "",
+			err:    errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrInvalidContact), http.StatusBadRequest),
 		},
 	}
 
 	for _, tc := range cases {
-		repoCall := auth.On("Identify", mock.Anything, &magistrala.IdentityReq{Token: tc.token}).Return(&magistrala.IdentityRes{Id: tc.userID}, tc.identifyErr)
-		repoCall1 := repo.On("Save", mock.Anything, mock.Anything).Return(tc.id, tc.err)
+		svcCall := nsvc.On("CreateSubscription", mock.Anything, tc.token, tc.svcReq).Return(tc.svcRes, tc.svcErr)
 		loc, err := mgsdk.CreateSubscription(tc.subscription.Topic, tc.subscription.Contact, tc.token)
 		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected error %s, got %s", tc.desc, tc.err, err))
 		assert.Equal(t, tc.empty, loc == "", fmt.Sprintf("%s: expected empty result location, got: %s", tc.desc, loc))
-		repoCall.Unset()
-		repoCall1.Unset()
+		if tc.err == nil {
+			ok := svcCall.Parent.AssertCalled(t, "CreateSubscription", mock.Anything, tc.token, tc.svcReq)
+			assert.True(t, ok, fmt.Sprintf("%s: expected CreateSubscription to be called", tc.desc))
+		}
+		svcCall.Unset()
 	}
 }
 
 func TestViewSubscription(t *testing.T) {
-	ts, auth, repo := setupSubscriptions()
+	ts, nsvc := setupSubscriptions()
 	defer ts.Close()
 	sdkConf := sdk.Config{
 		UsersURL:        ts.URL,
@@ -134,57 +162,67 @@ func TestViewSubscription(t *testing.T) {
 	mgsdk := sdk.NewSDK(sdkConf)
 
 	cases := []struct {
-		desc        string
-		subID       string
-		token       string
-		err         errors.SDKError
-		response    sdk.Subscription
-		identifyErr error
-		userID      string
+		desc     string
+		subID    string
+		token    string
+		svcRes   notifiers.Subscription
+		svcErr   error
+		response sdk.Subscription
+		err      errors.SDKError
 	}{
 		{
-			desc:        "get existing subscription",
-			subID:       ID,
-			token:       exampleUser1,
-			err:         nil,
-			response:    sub1,
-			identifyErr: nil,
-			userID:      validID,
+			desc:     "view existing subscription",
+			subID:    subID,
+			token:    validToken,
+			svcRes:   notSubRes,
+			svcErr:   nil,
+			response: sdkSubRes,
+			err:      nil,
 		},
 		{
-			desc:        "get non-existent subscription",
-			subID:       "43",
-			token:       exampleUser1,
-			err:         errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
-			response:    sdk.Subscription{},
-			identifyErr: nil,
-			userID:      validID,
+			desc:     "view non-existent subscription",
+			subID:    wrongID,
+			token:    validToken,
+			svcRes:   notifiers.Subscription{},
+			svcErr:   svcerr.ErrNotFound,
+			response: sdk.Subscription{},
+			err:      errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
 		},
 		{
-			desc:        "get subscription with invalid token",
-			subID:       ID,
-			token:       "",
-			err:         errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
-			response:    sdk.Subscription{},
-			identifyErr: svcerr.ErrAuthorization,
+			desc:     "view subscription with invalid token",
+			subID:    subID,
+			token:    invalidToken,
+			svcRes:   notifiers.Subscription{},
+			svcErr:   svcerr.ErrAuthentication,
+			response: sdk.Subscription{},
+			err:      errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
+		},
+		{
+			desc:     "view subscription with empty token",
+			subID:    subID,
+			token:    "",
+			svcRes:   notifiers.Subscription{},
+			svcErr:   nil,
+			response: sdk.Subscription{},
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
 		},
 	}
 
 	for _, tc := range cases {
-		repoCall := auth.On("Identify", mock.Anything, &magistrala.IdentityReq{Token: tc.token}).Return(&magistrala.IdentityRes{Id: tc.userID}, tc.identifyErr)
-		repoCall1 := repo.On("Retrieve", mock.Anything, mock.Anything).Return(notifiers.Subscription{Contact: sub1.Contact, Topic: sub1.Topic}, tc.err)
-		respSub, err := mgsdk.ViewSubscription(tc.subID, tc.token)
+		svcCall := nsvc.On("ViewSubscription", mock.Anything, tc.token, tc.subID).Return(tc.svcRes, tc.svcErr)
+		resp, err := mgsdk.ViewSubscription(tc.subID, tc.token)
 		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected error %s, got %s", tc.desc, tc.err, err))
-		tc.response.ID = respSub.ID
-		tc.response.OwnerID = respSub.OwnerID
-		assert.Equal(t, tc.response, respSub, fmt.Sprintf("%s: expected response %s, got %s", tc.desc, tc.response, respSub))
-		repoCall.Unset()
-		repoCall1.Unset()
+		assert.Equal(t, tc.response, resp, fmt.Sprintf("%s: expected response %s, got %s", tc.desc, tc.response, resp))
+		if tc.err == nil {
+			ok := svcCall.Parent.AssertCalled(t, "ViewSubscription", mock.Anything, tc.token, tc.subID)
+			assert.True(t, ok, fmt.Sprintf("%s: expected ViewSubscription to be called", tc.desc))
+		}
+		svcCall.Unset()
 	}
 }
 
 func TestListSubscription(t *testing.T) {
-	ts, auth, repo := setupSubscriptions()
+	ts, nsvc := setupSubscriptions()
 	defer ts.Close()
 	sdkConf := sdk.Config{
 		UsersURL:        ts.URL,
@@ -194,83 +232,157 @@ func TestListSubscription(t *testing.T) {
 
 	mgsdk := sdk.NewSDK(sdkConf)
 	nSubs := 10
-	subs := make([]sdk.Subscription, nSubs)
+	noSubs := []notifiers.Subscription{}
+	sdSubs := []sdk.Subscription{}
+	for i := 0; i < nSubs; i++ {
+		nosub := notifiers.Subscription{
+			OwnerID: ownerID,
+			Topic:   fmt.Sprintf("topic_%d", i),
+			Contact: fmt.Sprintf("contact_%d", i),
+		}
+		noSubs = append(noSubs, nosub)
+		sdsub := sdk.Subscription{
+			OwnerID: ownerID,
+			Topic:   fmt.Sprintf("topic_%d", i),
+			Contact: fmt.Sprintf("contact_%d", i),
+		}
+		sdSubs = append(sdSubs, sdsub)
+	}
 
 	cases := []struct {
-		desc        string
-		page        sdk.PageMetadata
-		token       string
-		err         errors.SDKError
-		response    []sdk.Subscription
-		Page        notifiers.Page
-		identifyErr error
-		userID      string
+		desc     string
+		token    string
+		pageMeta sdk.PageMetadata
+		svcReq   notifiers.PageMetadata
+		svcRes   notifiers.Page
+		svcErr   error
+		response sdk.SubscriptionPage
+		err      errors.SDKError
 	}{
 		{
-			desc:     "list all subscription",
-			token:    exampleUser1,
-			page:     sdk.PageMetadata{Offset: 0, Limit: uint64(nSubs)},
-			err:      nil,
-			response: subs,
-			Page: notifiers.Page{
+			desc:  "list all subscription",
+			token: validToken,
+			pageMeta: sdk.PageMetadata{
+				Offset: 0,
+				Limit:  10,
+			},
+			svcReq: notifiers.PageMetadata{
+				Offset: 0,
+				Limit:  10,
+			},
+			svcRes: notifiers.Page{
 				PageMetadata: notifiers.PageMetadata{
 					Offset: 0,
-					Limit:  nSubs,
+					Limit:  10,
 				},
-				Subscriptions: subSlice(subs, 0, 10),
+				Subscriptions: noSubs,
 			},
-			identifyErr: nil,
-			userID:      validID,
+			svcErr: nil,
+			response: sdk.SubscriptionPage{
+				Subscriptions: sdSubs,
+			},
+			err: nil,
 		},
 		{
-			desc:     "list subscription with specific topic",
-			token:    exampleUser1,
-			page:     sdk.PageMetadata{Offset: 0, Limit: uint64(nSubs), Topic: "topic_1"},
-			err:      nil,
-			response: []sdk.Subscription{subs[1]},
-			Page: notifiers.Page{
+			desc:  "list subscription with specific topic",
+			token: validToken,
+			pageMeta: sdk.PageMetadata{
+				Offset: 0,
+				Limit:  10,
+				Topic:  "topic_1",
+			},
+			svcReq: notifiers.PageMetadata{
+				Offset: 0,
+				Limit:  10,
+				Topic:  "topic_1",
+			},
+			svcRes: notifiers.Page{
 				PageMetadata: notifiers.PageMetadata{
 					Offset: 0,
-					Limit:  nSubs,
+					Limit:  10,
 					Topic:  "topic_1",
 				},
-				Subscriptions: subSlice(subs, 0, 1),
+				Subscriptions: noSubs[1:2],
 			},
-			identifyErr: nil,
-			userID:      validID,
+			svcErr: nil,
+			response: sdk.SubscriptionPage{
+				Subscriptions: sdSubs[1:2],
+			},
+			err: nil,
 		},
 		{
-			desc:     "list subscription with specific contact",
-			token:    exampleUser1,
-			page:     sdk.PageMetadata{Offset: 0, Limit: uint64(nSubs), Contact: "contact_1"},
-			err:      nil,
-			response: []sdk.Subscription{subs[1]},
-			Page: notifiers.Page{
+			desc:  "list subscription with specific contact",
+			token: validToken,
+			pageMeta: sdk.PageMetadata{
+				Offset:  0,
+				Limit:   10,
+				Contact: "contact_1",
+			},
+			svcReq: notifiers.PageMetadata{
+				Offset:  0,
+				Limit:   10,
+				Contact: "contact_1",
+			},
+			svcRes: notifiers.Page{
 				PageMetadata: notifiers.PageMetadata{
 					Offset:  0,
-					Limit:   nSubs,
+					Limit:   10,
 					Contact: "contact_1",
 				},
-				Subscriptions: subSlice(subs, 0, 1),
+				Subscriptions: noSubs[1:2],
 			},
-			identifyErr: nil,
-			userID:      validID,
+			svcErr: nil,
+			response: sdk.SubscriptionPage{
+				Subscriptions: sdSubs[1:2],
+			},
+			err: nil,
+		},
+		{
+			desc:  "list subscription with invalid token",
+			token: invalidToken,
+			pageMeta: sdk.PageMetadata{
+				Offset: 0,
+				Limit:  10,
+			},
+			svcReq: notifiers.PageMetadata{
+				Offset: 0,
+				Limit:  10,
+			},
+			svcRes:   notifiers.Page{},
+			svcErr:   svcerr.ErrAuthentication,
+			response: sdk.SubscriptionPage{},
+			err:      errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
+		},
+		{
+			desc:  "list subscription with empty token",
+			token: "",
+			pageMeta: sdk.PageMetadata{
+				Offset: 0,
+				Limit:  10,
+			},
+			svcReq:   notifiers.PageMetadata{},
+			svcRes:   notifiers.Page{},
+			svcErr:   nil,
+			response: sdk.SubscriptionPage{},
+			err:      errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
 		},
 	}
 
 	for _, tc := range cases {
-		repoCall := auth.On("Identify", mock.Anything, &magistrala.IdentityReq{Token: tc.token}).Return(&magistrala.IdentityRes{Id: tc.userID}, tc.identifyErr)
-		repoCall1 := repo.On("RetrieveAll", mock.Anything, mock.Anything).Return(tc.Page, tc.err)
-		subs, err := mgsdk.ListSubscriptions(tc.page, tc.token)
+		svcCall := nsvc.On("ListSubscriptions", mock.Anything, tc.token, tc.svcReq).Return(tc.svcRes, tc.svcErr)
+		resp, err := mgsdk.ListSubscriptions(tc.pageMeta, tc.token)
 		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected error %s, got %s", tc.desc, tc.err, err))
-		assert.Equal(t, tc.response, subs.Subscriptions, fmt.Sprintf("%s: expected response %v, got %v", tc.desc, tc.response, subs.Subscriptions))
-		repoCall.Unset()
-		repoCall1.Unset()
+		assert.Equal(t, tc.response.Subscriptions, resp.Subscriptions, fmt.Sprintf("%s: expected response %v, got %v", tc.desc, tc.response, resp.Subscriptions))
+		if tc.err == nil {
+			ok := svcCall.Parent.AssertCalled(t, "ListSubscriptions", mock.Anything, tc.token, tc.svcReq)
+			assert.True(t, ok, fmt.Sprintf("%s: expected ListSubscriptions to be called", tc.desc))
+		}
+		svcCall.Unset()
 	}
 }
 
 func TestDeleteSubscription(t *testing.T) {
-	ts, auth, repo := setupSubscriptions()
+	ts, nsvc := setupSubscriptions()
 	defer ts.Close()
 	sdkConf := sdk.Config{
 		UsersURL:        ts.URL,
@@ -281,62 +393,57 @@ func TestDeleteSubscription(t *testing.T) {
 	mgsdk := sdk.NewSDK(sdkConf)
 
 	cases := []struct {
-		desc        string
-		subID       string
-		token       string
-		err         errors.SDKError
-		response    sdk.Subscription
-		identifyErr error
-		userID      string
+		desc   string
+		subID  string
+		token  string
+		svcErr error
+		err    errors.SDKError
 	}{
 		{
-			desc:        "delete existing subscription",
-			subID:       ID,
-			token:       exampleUser1,
-			err:         nil,
-			response:    sub1,
-			identifyErr: nil,
-			userID:      validID,
+			desc:   "delete existing subscription",
+			subID:  subID,
+			token:  validToken,
+			svcErr: nil,
+			err:    nil,
 		},
 		{
-			desc:        "delete non-existent subscription",
-			subID:       "43",
-			token:       exampleUser1,
-			err:         errors.NewSDKErrorWithStatus(svcerr.ErrNotFound, http.StatusNotFound),
-			response:    sdk.Subscription{},
-			identifyErr: nil,
-			userID:      validID,
+			desc:   "delete non-existent subscription",
+			subID:  wrongID,
+			token:  validToken,
+			svcErr: svcerr.ErrRemoveEntity,
+			err:    errors.NewSDKErrorWithStatus(svcerr.ErrRemoveEntity, http.StatusUnprocessableEntity),
 		},
 		{
-			desc:        "delete subscription with invalid token",
-			subID:       ID,
-			token:       "",
-			err:         errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
-			response:    sdk.Subscription{},
-			identifyErr: svcerr.ErrAuthorization,
+			desc:   "delete subscription with invalid token",
+			subID:  subID,
+			token:  invalidToken,
+			svcErr: svcerr.ErrAuthentication,
+			err:    errors.NewSDKErrorWithStatus(svcerr.ErrAuthentication, http.StatusUnauthorized),
+		},
+		{
+			desc:   "delete subscription with empty token",
+			subID:  subID,
+			token:  "",
+			svcErr: nil,
+			err:    errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrBearerToken), http.StatusUnauthorized),
+		},
+		{
+			desc:   "delete subscription with empty subID",
+			subID:  "",
+			token:  validToken,
+			svcErr: nil,
+			err:    errors.NewSDKErrorWithStatus(errors.Wrap(apiutil.ErrValidation, apiutil.ErrMissingID), http.StatusBadRequest),
 		},
 	}
 
 	for _, tc := range cases {
-		repoCall := auth.On("Identify", mock.Anything, &magistrala.IdentityReq{Token: tc.token}).Return(&magistrala.IdentityRes{Id: tc.userID}, tc.err)
-		repoCall1 := repo.On("Remove", mock.Anything, mock.Anything).Return(tc.err)
+		svcCall := nsvc.On("RemoveSubscription", mock.Anything, tc.token, tc.subID).Return(tc.svcErr)
 		err := mgsdk.DeleteSubscription(tc.subID, tc.token)
 		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: expected error %s, got %s", tc.desc, tc.err, err))
-		repoCall.Unset()
-		repoCall1.Unset()
+		if tc.err == nil {
+			ok := svcCall.Parent.AssertCalled(t, "RemoveSubscription", mock.Anything, tc.token, tc.subID)
+			assert.True(t, ok, fmt.Sprintf("%s: expected RemoveSubscription to be called", tc.desc))
+		}
+		svcCall.Unset()
 	}
-}
-
-func subSlice(subs []sdk.Subscription, start, end int) []notifiers.Subscription {
-	var res []notifiers.Subscription
-	for i := start; i < end; i++ {
-		sub := subs[i]
-		res = append(res, notifiers.Subscription{
-			ID:      sub.ID,
-			OwnerID: sub.OwnerID,
-			Contact: sub.Contact,
-			Topic:   sub.Topic,
-		})
-	}
-	return res
 }
