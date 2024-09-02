ad51738bf9e5ada1cf65306f497e25669d097c21
1998-felix <felix.gateru@gmail.com>
2024-09-02 19:34:14 +0300

    refactor: refactor client type in adapters
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

--- certs/service.go
+++ certs/service.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/absmach/magistrala"
-	grpcclient "github.com/absmach/magistrala/auth/api/grpc"
 	"github.com/absmach/magistrala/certs/pki"
 	"github.com/absmach/magistrala/pkg/errors"
 	svcerr "github.com/absmach/magistrala/pkg/errors/service"
