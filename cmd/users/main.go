66bc098f0c5090304790957e0433223505e06a18
1998-felix <felix.gateru@gmail.com>
2024-09-06 10:38:36 +0300

    refactor: rebase
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -25,8 +25,6 @@ import (
 	gpostgres "github.com/absmach/magistrala/internal/groups/postgres"
 	gtracing "github.com/absmach/magistrala/internal/groups/tracing"
 	mgpolicy "github.com/absmach/magistrala/internal/policy"
-	"github.com/absmach/magistrala/internal/policy/agent"
-	"github.com/absmach/magistrala/internal/policy/middleware"
 	mglog "github.com/absmach/magistrala/logger"
 	mgclients "github.com/absmach/magistrala/pkg/clients"
 	"github.com/absmach/magistrala/pkg/groups"
@@ -264,7 +262,7 @@ func newService(ctx context.Context, authClient authclient.AuthServiceClient, au
 
 	csvc = ctracing.New(csvc, tracer)
 	csvc = capi.LoggingMiddleware(csvc, logger)
-	counter, latency = prometheus.MakeMetrics(svcName, "api")
+	counter, latency := prometheus.MakeMetrics(svcName, "api")
 	csvc = capi.MetricsMiddleware(csvc, counter, latency)
 
 	gsvc = gtracing.New(gsvc, tracer)
