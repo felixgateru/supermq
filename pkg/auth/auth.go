15ba26de18e65347a8fd424b49e44a6da1dbd670
1998-felix <felix.gateru@gmail.com>
2024-09-06 14:18:21 +0300

    feat: move client implementation to internal/auth
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -7,40 +7,23 @@ import (
 	"context"
 
 	"github.com/absmach/magistrala"
+	"google.golang.org/grpc"
 )
 
-type Token struct {
-	AccessToken  string // AccessToken contains the security credentials for a login session and identifies the client.
-	RefreshToken string // RefreshToken is a credential artifact that OAuth can use to get a new access token without client interaction.
-	AccessType   string // AccessType is the specific type of access token issued. It can be Bearer, Client or Basic.
-}
-
-type IdentityRes struct {
-	ID       string
-	UserID   string
-	DomainID string
-}
-
-type AuthorizeRes struct {
-	Authorized bool
-	ID         string
-}
-
-// AuthWrapper specifies an API for interacting with authentication and authorization for magistrala services.
-// Acts a wrapper for services that communicates with the magistrala auth service.
-type AuthWrapper interface {
+// AuthClient specifies a gRPC client for  authentication and authorization for magistrala services.
+type AuthClient interface {
 	// Issue issues a new Key, returning its token value alongside.
-	Issue(ctx context.Context, req *magistrala.IssueReq) (Token, error)
+	Issue(ctx context.Context, in *magistrala.IssueReq, opts ...grpc.CallOption) (*magistrala.Token, error)
 
 	// Refresh iisues a refresh Key, returning its token value alongside.
-	Refresh(ctx context.Context, req *magistrala.RefreshReq) (Token, error)
+	Refresh(ctx context.Context, in *magistrala.RefreshReq, opts ...grpc.CallOption) (*magistrala.Token, error)
 
 	// Identify validates token token. If token is valid, content
 	// is returned. If token is invalid, or invocation failed for some
 	// other reason, non-nil error value is returned in response.
-	Identify(ctx context.Context, token *magistrala.IdentityReq) (IdentityRes, error)
+	Identify(ctx context.Context, in *magistrala.IdentityReq, opts ...grpc.CallOption) (*magistrala.IdentityRes, error)
 
 	// Authorize checks if the `subject` is allowed to perform the `relation` on the `object`.
 	// Returns a non-nil error if the `subject` is not authorized.
-	Authorize(ctx context.Context, req *magistrala.AuthorizeReq) (AuthorizeRes, error)
+	Authorize(ctx context.Context, in *magistrala.AuthorizeReq, opts ...grpc.CallOption) (*magistrala.AuthorizeRes, error)
 }
