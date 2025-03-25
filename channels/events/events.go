6d8ca5521baf373cd9b2f307ea046f3312e658d6
Felix Gateru <felix.gateru@gmail.com>
2025-03-25 16:39:49 +0300

    feat: add topic to channels
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

--- channels/events/events.go
+++ channels/events/events.go
@@ -50,6 +50,7 @@ func (cce createChannelEvent) Encode() (map[string]interface{}, error) {
 		"operation":         channelCreate,
 		"id":                cce.ID,
 		"roles_provisioned": cce.rolesProvisioned,
+		"topic":             cce.Topic,
 		"status":            cce.Status.String(),
 		"created_at":        cce.CreatedAt,
 		"domain":            cce.DomainID,
@@ -93,7 +94,9 @@ func (uce updateChannelEvent) Encode() (map[string]interface{}, error) {
 	if uce.operation != "" {
 		val["operation"] = channelUpdate + "_" + uce.operation
 	}
-
+	if uce.Topic != "" {
+		val["topic"] = uce.Topic
+	}
 	if uce.ID != "" {
 		val["id"] = uce.ID
 	}
@@ -159,6 +162,9 @@ func (vce viewChannelEvent) Encode() (map[string]interface{}, error) {
 
 	if vce.Name != "" {
 		val["name"] = vce.Name
+	}
+	if vce.Topic != "" {
+		val["topic"] = vce.Topic
 	}
 	if len(vce.Tags) > 0 {
 		val["tags"] = vce.Tags
--- channels/mocks/repository.go
+++ channels/mocks/repository.go
@@ -478,6 +478,34 @@ func (_m *Repository) RetrieveByIDWithRoles(ctx context.Context, id string, memb
 	return r0, r1
 }
 
+// RetrieveByTopic provides a mock function with given fields: ctx, topic, domainID
+func (_m *Repository) RetrieveByTopic(ctx context.Context, topic string, domainID string) (channels.Channel, error) {
+	ret := _m.Called(ctx, topic, domainID)
+
+	if len(ret) == 0 {
+		panic("no return value specified for RetrieveByTopic")
+	}
+
+	var r0 channels.Channel
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, string, string) (channels.Channel, error)); ok {
+		return rf(ctx, topic, domainID)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, string, string) channels.Channel); ok {
+		r0 = rf(ctx, topic, domainID)
+	} else {
+		r0 = ret.Get(0).(channels.Channel)
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, string, string) error); ok {
+		r1 = rf(ctx, topic, domainID)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // RetrieveEntitiesRolesActionsMembers provides a mock function with given fields: ctx, entityIDs
 func (_m *Repository) RetrieveEntitiesRolesActionsMembers(ctx context.Context, entityIDs []string) ([]roles.EntityActionRole, []roles.EntityMemberRole, error) {
 	ret := _m.Called(ctx, entityIDs)
