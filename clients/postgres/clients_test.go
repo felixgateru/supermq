c589dd27841dc751c570748e7ee9c9910dd7f673
Felix Gateru <felix.gateru@gmail.com>
2025-12-04 00:44:36 +0300

    tests: update tests
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

--- clients/postgres/clients_test.go
+++ clients/postgres/clients_test.go
@@ -2225,7 +2225,7 @@ func TestRetrieveUserClients(t *testing.T) {
 			pm: clients.Page{
 				Offset: 0,
 				Limit:  nClients,
-				Tag:    directClients[0].Tags[0],
+				Tags:   clients.TagsQuery{Elements: []string{directClients[0].Tags[0]}, Operator: clients.OrOp},
 				Status: clients.AllStatus,
 				Order:  defOrder,
 				Dir:    ascDir,
@@ -2246,7 +2246,7 @@ func TestRetrieveUserClients(t *testing.T) {
 			pm: clients.Page{
 				Offset: 0,
 				Limit:  nClients,
-				Tag:    namegen.Generate(),
+				Tags:   clients.TagsQuery{Elements: []string{namegen.Generate()}, Operator: clients.OrOp},
 				Status: clients.AllStatus,
 				Order:  defOrder,
 				Dir:    ascDir,
@@ -2269,7 +2269,7 @@ func TestRetrieveUserClients(t *testing.T) {
 				Limit:    nClients,
 				Metadata: directClients[0].Metadata,
 				Name:     directClients[0].Name,
-				Tag:      directClients[0].Tags[0],
+				Tags:     clients.TagsQuery{Elements: []string{directClients[0].Tags[0]}, Operator: clients.OrOp},
 				Identity: directClients[0].Credentials.Identity,
 				Status:   clients.AllStatus,
 			},
